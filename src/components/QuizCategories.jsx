2842df54916069209d9cc913e54cc626932a3d01
mgbams <cmgbams@gmail.com>
2021-05-19 00:09:43 +0200

    fetch data from API

--- src/components/QuizCategories.jsx
+++ src/components/QuizCategories.jsx
@@ -1,7 +1,36 @@
-import React from "react";
 import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function QuizCategories() {
+  const [categories, setCategories] = useState([]);
+  //generated from Trivia API
+  const getQuiz = async () => {
+    const { data } = await axios.get(
+      "https://opentdb.com/api.php?amount=20&category=18&difficulty=medium"
+    );
+
+    const formattedData = data.results.map((category) => {
+      const incorrectAnswersIndexes = category.incorrect_answers.length;
+      const randomIndex = Math.random() * (incorrectAnswersIndexes - 0) + 0;
+      category.incorrect_answers.splice(
+        randomIndex,
+        0,
+        category.correct_answer
+      );
+      return {
+        ...category,
+        answes: category.incorrect_answers,
+      };
+    });
+
+    setCategories(formattedData);
+  };
+
+  useEffect(() => {
+    getQuiz();
+  }, []);
+
+  console.log(categories);
   return <div>Quiz Categories</div>;
 }
 
